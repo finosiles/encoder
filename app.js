1bcfba761d8dac8b6a36e21f9d714b3451c1de3d
Rufino Siles <rfinosiles@gmail.com>
2022-07-16 21:25:38 -0300

    cerrando actualizacion

--- app.js
+++ app.js
@@ -1,52 +1,38 @@
-
+window.onload=inicio;
 // Controles de los eventos de la pagina
 
-function escribiendo(){
-    alert("Esta escribiendo");
+
+
+function inicio(){
+    document.querySelectorAll("button")[0].onclick=cifrar;
+    document.querySelectorAll("button")[1].onclick=descifrar;
+    
 }
-
-
-// Desde aqui estoy obteniendo el valor del texto
-
-let texto ;
-
-texto = document.getElementById("textoobtenido").value;
-
-// Tomaremos dicho contenido y lo enviaremos a un array
-
-let textoPorpalabras = texto.split(' ')
-
-console.log(textoPorpalabras.length);
-
+let cifrado ;
+document.querySelector(".textos1").addEventListener("click", function(){
+    let elemento = document.querySelector(".muñeco");
+    elemento.classList.add('ocultar');
+})
 function cifrar(){
-    for(let k = 0; k < textoPorpalabras.length ; k++ ){
-	let cifrado;
-	cifrado  =textoPorpalabras[k]
-	    .replaceAll('a','ai')
-    	    .replaceAll('e','enter')
-    	    .replaceAll('i','imes')
-    	    .replaceAll('o','ober')
-    	    .replaceAll('u','ufat');  
-	console.log(cifrado);
-	
-    }
+    let escrito=document.querySelector("#textoobtenido").value;
+    cifrado  = escrito
+	.replace(/i/gi,'imes')
+	.replace(/a/gi,'ai')
+	.replace(/e/gi,'enter')
+    	.replace(/o/gi,'ober')
+	.replace(/u/gi,'ufat');
+    document.querySelector(".salida").value = cifrado;
 }
-
-
-
-
-// textoPorpalabras.forEach(function(numero){
-//     if(numero = )
-// })
-
-
-
-let cifra = texto.replaceAll('a','ai');
-let cifre = cifra.replaceAll('e','enter');
-let cifri = cifre.replaceAll('i','imes');
-let cifro = cifri.replaceAll('o','ober');
-let cifru = cifro.replaceAll('u','ufat');
-
+function descifrar(){
+    let escrito=document.querySelector("#textoobtenido").value;
+    cifrado  = escrito
+	.replace(/imes/gi,'i')
+	.replace(/ai/gi,'a')
+	.replace(/enter/gi,'e')
+	.replace(/ober/gi,'o')
+	.replace(/ufat/gi,'u');
+    document.querySelector(".salida").value = cifrado;
+}
 
 
 
